27fe49fe446cc96b8d683f4cf27fe034c1751a6c
Thomas Woffenden <t@omwo.uk>
2016-01-06 13:42:45 +0000

    #

--- 001/001.php
+++ 001/001.php
@@ -1,4 +1,36 @@
 #!/usr/bin/php
 <?php
-echo "hello world\n";
+
+$x = 1000;
+
+$sum = 0;
+
+$multiples[] = 0;
+
+while($x > 0)
+{
+--$x;
+        if(($x % 3 == 0))
+        {
+        echo "$x is a multiple of 3\n";
+        $sum+=$x;
+	array_push($multiples, $x);
+	}
+
+        if($x % 5 == 0)
+        {
+        echo "$x is a multiple of 5\n";
+        $sum+=$x;
+	array_push($multiples, $x);
+        }
+
+        if(($x % 3 == 0)&&($x % 5 == 0))
+        {
+        echo "$x is a multiple of both\n";
+        $sum-=$x;
+        }
+}
+
+echo "$sum\n";
+
 ?>
