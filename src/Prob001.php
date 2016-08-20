4c9eeb11bad44f3dc5349718fc4f8f4daa4b4dc8
Thomas Woffenden <t@omwo.uk>
2016-08-20 18:04:45 +0100

    Introduced more state

---
+++ src/Prob001.php
@@ -0,0 +1,38 @@
+<?php
+class Prob001 {
+    private $a;
+    private $n;
+    private $multiples;
+    public $sum;
+
+    public function __construct($a,$n)
+    { // n = divisor & a = dividend
+      $this->a = $a;
+      $this->n = $n;
+      $this->multiples = [];
+      $this->sum = 0;
+      $this->findMultiples();
+      $this->sumMultiples();
+    }
+
+    public function isMultipleOf($a,$n)
+    {
+        if($a % $n == 0)
+          return true;
+        else
+          return false;
+    }
+
+    public function findMultiples()
+    {
+        foreach ($this->a as $a)
+          foreach ($this->n as $n)
+            if ($this->isMultipleOf($a,$n))
+              array_push($this->multiples,$a);
+    }
+
+    public function sumMultiples()
+    {
+      $this->sum = array_sum(array_unique($this->multiples));
+    }
+}
---
+++ tests/Prob001Test.php
@@ -0,0 +1,25 @@
+<?php
+include_once __DIR__ . '/../src/Prob001.php';
+
+class Prob001Test extends \PHPUnit_Framework_TestCase
+{
+    public function testIsMultipleOf()
+    {
+        $prob001 = new Prob001(range(1,999),array(3,5));
+        $this->assertTrue($prob001->isMultipleOf(6,3));
+        $this->assertFalse($prob001->isMultipleOf(6,4));
+    }
+
+    public function testSumMultiples()
+    {
+      $prob001 = new Prob001(range(1,999),array(3,5));
+      $this->assertEquals(233168,$prob001->sum);
+    }
+  }
+/*
+Multiples of 3 and 5
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
